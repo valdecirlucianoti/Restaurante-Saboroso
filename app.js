468d09774dc96a7ce015c060c8b5227b74eec333
Valdecir Luciano <valdecirlucianoti@gmail.com>
2021-08-23 23:00:05 -0300

    Implementado sistema de login e sessão de usuario

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var createError = require('http-errors');
 var express = require('express');
+const redis = require('redis');
+const session = require('express-session');
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient();
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index'); 
 var adminRouter = require('./routes/admin');
 
 var app = express();
@@ -14,23 +18,31 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: 'password',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,25 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var users = require('./../inc/users');
+
 var router = express.Router();
 
+router.use(function (req, res, next) {
+    if (['/login', '/lostpassword'].indexOf(req.url) === -1 && !req.session.user) {
+        users.render(req, res, "Necessario Autenticação !");
+    } else {
+        next();
+    }
+
+
+});
+
 /* GET users listing. */
+router.get('/logout', function (req, res, next) {
+    delete req.session.user;
+    res.redirect('admin/login');
+});
+
 router.get('/', function (req, res, next) {
 
     res.render("admin/index", {
@@ -11,11 +28,30 @@ router.get('/', function (req, res, next) {
 
 });
 
+router.post('/login', function (req, res, next) {
+
+    if (!req.body.email) {
+        users.render(req, res, "Preencha o campo e-mail.");
+    } else if (!req.body.password) {
+        users.render(req, res, "Preencha o campo password.");
+    } else {
+        users.login(req.body.email, req.body.password).then(user => {
+            req.session.user = user;
+            res.redirect('/admin');
+        }).catch(err => {
+            users.render(req, res, err.message || err);
+        });
+    }
+
+});
+
 router.get('/login', function (req, res, next) {
 
-    res.render("admin/login", {
+    if (!req.session.views) req.session.views = 0;
 
-    });
+    console.log("SESSION: ", req.session.views++);
+
+    users.render(req, res);
 
 });
 
@@ -30,7 +66,7 @@ router.get('/users', function (req, res, next) {
 router.get('/reservations', function (req, res, next) {
 
     res.render("admin/reservations", {
-
+        date: {}
     });
 
 });
